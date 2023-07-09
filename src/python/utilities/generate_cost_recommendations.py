c010c2b1ad8082288c7ec2490ef1b8ac293cc64b
roeeagiman <53697891+roeeagiman@users.noreply.github.com>
2023-07-09 13:27:15 +0300

    generate_cost_recommendations.py: Removed "accounts" argument

--- src/python/utilities/generate_cost_recommendations.py
+++ src/python/utilities/generate_cost_recommendations.py
@@ -16,10 +16,6 @@ except ModuleNotFoundError:
 
 
 def main(environment, ll_username, ll_password, ws_name, accounts):
-    # Setting up variables
-    if accounts:
-        accounts = accounts.replace(" ", "").split(",")
-
     print(color("Trying to login into Lightlytics", "blue"))
     ll_url = f"https://{environment}.lightlytics.com"
     ll_graph_url = f"{ll_url}/graphql"
@@ -69,8 +65,6 @@ if __name__ == "__main__":
         "--environment_password", help="The Lightlytics environment password", required=True)
     parser.add_argument(
         "--ws_name", help="The WS from which to fetch information", required=True)
-    parser.add_argument(
-        "--accounts", help="Accounts list to iterate when creating the report", required=False)
     args = parser.parse_args()
     main(args.environment_sub_domain, args.environment_user_name, args.environment_password,
          args.ws_name, args.accounts)
