67230eb4480c10aad4ad2bbdeded2e3c90be60b6
Roee Agiman <roee@lightlytics.com>
2023-08-16 17:39:58 +0300

    generate_compliance_report.py: Added logging and API access main.py: Added background tasks and added compliance report requirements.txt: Added "starlette"

--- main.py
+++ main.py
@@ -1,41 +1,31 @@
 import os
 import uvicorn
-from fastapi import FastAPI, Request, Response, HTTPException
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import StreamingResponse, FileResponse
 from typing import Dict, Any
 from src.python.common.logger import Logger
+from starlette.background import BackgroundTasks
 app = FastAPI()
 log = Logger().get_logger()
 
 from src.python.utilities import generate_cost_report as cost_report
 from src.python.utilities import generate_cost_recommendations as cost_recommendations
-
-
-@app.middleware("http")
-async def after_request(request: Request, call_next):
-    response: Response = await call_next(request)
-    log.info(f"Cleaning up CSV files from the main directory")
-    directory = os.getcwd()
-    files = os.listdir(directory)
-    for f in files:
-        if f.endswith(".csv"):
-            os.remove(os.path.join(directory, f))
-    return response
+from src.python.utilities import generate_compliance_report as compliance_report
 
 
 @app.post("/generate_cost_report")
-def generate_cost_report(payload: Dict[Any, Any]):
-    log.info(f"Generate Cost Report requested - {payload['environment_sub_domain']}")
+def generate_cost_report(payload: Dict[Any, Any], background_tasks: BackgroundTasks):
+    log.info(f"### Generate Cost Report requested - {payload['environment_sub_domain']}")
     arguments = [payload['environment_sub_domain'], payload['environment_user_name'], payload['environment_password'],
-                 payload['ws_name'], payload['start_timestamp'], payload['end_timestamp'], payload['period']]
-    if 'stage' in payload:
-        arguments.append(payload['stage'])
+                 payload['ws_name'], payload['start_timestamp'], payload['end_timestamp'], payload['period'],
+                 payload.get('stage', None)]
     try:
         file_name = cost_report.main(*arguments)
         headers = {
             'Content-Type': 'text/csv',
             'Content-Disposition': f'attachment; filename="{file_name}"'
         }
+        background_tasks.add_task(remove_file, file_name)
         with open(file_name) as csv_file:
             return StreamingResponse(iter([csv_file.read()]), headers=headers)
     except Exception as e:
@@ -43,22 +33,41 @@ def generate_cost_report(payload: Dict[Any, Any]):
 
 
 @app.post("/generate_cost_recommendations")
-def generate_cost_recommendations(payload: Dict[Any, Any]):
-    log.info(f"Generate Cost Recommendations requested - {payload['environment_sub_domain']}")
+def generate_cost_recommendations(payload: Dict[Any, Any], background_tasks: BackgroundTasks):
+    log.info(f"### Generate Cost Recommendations requested - {payload['environment_sub_domain']}")
     arguments = [payload['environment_sub_domain'], payload['environment_user_name'], payload['environment_password'],
-                 payload['ws_name']]
-    if 'stage' in payload:
-        arguments.append(payload['stage'])
+                 payload['ws_name'], payload.get('stage', None)]
     try:
         file_name = cost_recommendations.main(*arguments)
         headers = {
             'Content-Type': 'text/csv',
             'Content-Disposition': f'attachment; filename="{file_name}"'
         }
+        background_tasks.add_task(remove_file, file_name)
         with open(file_name) as csv_file:
             return StreamingResponse(iter([csv_file.read()]), headers=headers)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/generate_compliance_report")
+async def generate_compliance_report(payload: Dict[Any, Any], background_tasks: BackgroundTasks):
+    log.info(f"### Generate Compliance Report requested - {payload['environment_sub_domain']}")
+    arguments = [payload['environment_sub_domain'], payload['environment_user_name'], payload['environment_password'],
+                 payload['ws_name'], payload['compliance_standard'],
+                 payload.get('accounts', None), payload.get('label', None), payload.get('stage', None)]
+    try:
+        file_name = compliance_report.main(*arguments)
+        headers = {'Content-Disposition': f'attachment; filename="{file_name}"'}
+        background_tasks.add_task(remove_file, file_name)
+        return FileResponse(file_name, headers=headers)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+def remove_file(path: str) -> None:
+    log.info(f'Removing file "{path}"')
+    os.unlink(path)
 
 
 if __name__ == "__main__":
--- src/python/utilities/generate_compliance_report.py
+++ src/python/utilities/generate_compliance_report.py
@@ -1,4 +1,5 @@
 import argparse
+import concurrent.futures
 import itertools
 import os
 import sys
@@ -9,61 +10,54 @@ from urllib.parse import quote_plus
 # Add the project root directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
 try:
-    from src.python.common.boto_common import *
-    from src.python.common.graph_common import GraphCommon
+    from src.python.common.common import *
     from src.python.common.xlsx_tools import XlsxFile
 except ModuleNotFoundError:
     sys.path.append("../../..")
-    from src.python.common.boto_common import *
-    from src.python.common.graph_common import GraphCommon
+    from src.python.common.common import *
     from src.python.common.xlsx_tools import XlsxFile
 
 
-def main(environment, ll_username, ll_password, ws_name, compliance, accounts, label):
+def main(environment, ll_username, ll_password, ws_name, compliance, accounts=None, label=None, stage=None):
     # Setting up variables
     if accounts:
         accounts = accounts.replace(" ", "").split(",")
 
-    print(color("Trying to login into Lightlytics", "blue"))
-    ll_url = f"https://{environment}.lightlytics.com"
-    ll_graph_url = f"{ll_url}/graphql"
-    graph_client = GraphCommon(ll_graph_url, ll_username, ll_password)
-    ws_id = graph_client.get_ws_id_by_name(ws_name)
-    graph_client.change_client_ws(ws_id)
-    print(color("Logged in successfully!", "green"))
+    # Connecting to Lightlytics
+    graph_client = get_graph_client(environment, ll_username, ll_password, ws_name, stage)
 
-    print(color(f"Verifying that '{compliance}' compliance standard exist", "blue"))
+    log.info(f"Verifying that '{compliance}' compliance standard exist")
     compliance_list = graph_client.get_compliance_standards()
     if compliance not in compliance_list:
-        print(color(f'Compliance "{compliance}" not found', "red"))
-        print(color(f"Available compliance standards: {compliance_list}", "blue"))
+        log.error(f'Compliance "{compliance}" not found')
+        log.info(f"Available compliance standards: {compliance_list}")
         raise Exception(f"Unsupported compliance standard: {compliance}")
-    print(color("Compliance standard OK!", "green"))
+    log.info("Compliance standard OK!")
 
-    print(color(f"Getting all compliance rules", "blue"))
+    log.info(f"Getting all compliance rules")
     compliance_rules = graph_client.get_rules_by_compliance(compliance)
     compliance_rules_count = len(compliance_rules)
-    print(color(f"Found {compliance_rules_count} compliance rules!", "green"))
+    log.info(f"Found {compliance_rules_count} compliance rules!")
 
     if label:
-        print(color(f"Verifying that this label exist: '{label}'", "blue"))
+        log.info(f"Verifying that this label exist: '{label}'")
         all_labels = list(set(itertools.chain.from_iterable([c["labels"] for c in compliance_rules])))
         if label not in all_labels:
             err_msg = f"Can't find label: '{label}'"
-            print(color(err_msg, "red"))
-            print(color(f"Available labels: {all_labels}", "blue"))
+            log.error(err_msg)
+            log.info(f"Available labels: {all_labels}")
             raise Exception(err_msg)
         else:
-            print(color(f"Filtering rules using this label: '{label}'", "blue"))
+            log.info(f"Filtering rules using this label: '{label}'")
             compliance_rules = [c for c in compliance_rules if label in c["labels"]]
             compliance_rules_count = len(compliance_rules)
-            print(color(f"There are {compliance_rules_count} compliance rules matching the label '{label}'", "green"))
+            log.info(f"There are {compliance_rules_count} compliance rules matching the label '{label}'")
 
     report_details = {
         "environment_name": environment.upper(),
         "environment_workspace": ws_name,
-        "ws_id": ws_id,
-        "ll_url": ll_url,
+        "ws_id": graph_client.customer_id,
+        "ll_url": graph_client.url,
         "compliance_name": compliance.upper(),
         "compliance_label": label,
         "generation_date": date.today().strftime("%d/%m/%Y"),
@@ -74,15 +68,15 @@ def main(environment, ll_username, ll_password, ws_name, compliance, accounts, l
         "violated_rules": []
     }
 
-    print(color(f"Getting violations for each rule", "blue"))
+    log.info(f"Getting violations for each rule")
     for rule in compliance_rules:
         rule["violations"] = graph_client.get_rule_violations(rule["id"])
         violations_count = len(rule["violations"])
         rule["metadata"] = graph_client.get_rule_metadata(rule["id"])
         report_details["total_violations"] += violations_count
         if violations_count > 0:
-            print(color(f"Generating report for rule '{rule['name']}'", "blue"))
-            print(color(f"Adding report for {violations_count} violations", "blue"))
+            log.info(f"Generating report for rule '{rule['name']}'")
+            log.info(f"Adding report for {violations_count} violations")
 
             # Create a ThreadPoolExecutor
             with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -91,7 +85,8 @@ def main(environment, ll_username, ll_password, ws_name, compliance, accounts, l
                 # Iterate over the violations
                 for violation in rule["violations"]:
                     # Submit each violation to the executor and store the future object
-                    future = executor.submit(process_violation, violation, graph_client, ll_url, ws_id)
+                    future = executor.submit(
+                        process_violation, violation, graph_client, graph_client.url, graph_client.customer_id)
                     futures.append(future)
                 # Create a dictionary to store the results
                 rule_details = {
@@ -129,23 +124,23 @@ def main(environment, ll_username, ll_password, ws_name, compliance, accounts, l
 
             report_details["violated_rules"].append(rule_details)
             report_details["total_rules_violated"] += 1
-            print(color(f"Finished generating report for rule '{rule['name']}'!", "green"))
+            log.info(f"Finished generating report for rule '{rule['name']}'!")
 
-    print(color("Getting accounts list from the workspace", "blue"))
+    log.info("Getting accounts list from the workspace")
     ws_accounts = graph_client.get_accounts()
-    print(color(f"Accounts in the selected workspace: {[a['cloud_account_id'] for a in ws_accounts]}", "green"))
+    log.info(f"Accounts in the selected workspace: {[a['cloud_account_id'] for a in ws_accounts]}")
 
     # Filtering accounts according to parameter
     if accounts:
         ws_accounts = [a for a in ws_accounts if a['cloud_account_id'] in accounts]
-        print(color(f"Accounts included in the report: {[a['cloud_account_id'] for a in ws_accounts]}", "blue"))
+        log.info(f"Accounts included in the report: {[a['cloud_account_id'] for a in ws_accounts]}")
     report_details["total_accounts"] = len(ws_accounts)
 
-    print(color("Enriching rules with accounts information", "blue"))
+    log.info("Enriching rules with accounts information")
     enrich_accounts(report_details, ws_accounts, graph_client)
-    print(color("Enriching finished successfully", "green"))
+    log.info("Enriching finished successfully")
 
-    print(color("Generating XLSX file", "blue"))
+    log.info("Generating XLSX file")
     xlsx_file_name = f"{environment.upper()} {compliance}{f' {label}' if label else ''} Compliance report.xlsx"
     xlsx = XlsxFile(xlsx_file_name)
     xlsx.create_compliance_report_template(report_details)
@@ -153,6 +148,8 @@ def main(environment, ll_username, ll_password, ws_name, compliance, accounts, l
         rule_number = i + 1
         xlsx.create_new_rule_sheet(report_details, violated_rule, rule_number, ws_accounts)
     xlsx.save_xlsx()
+
+    return xlsx_file_name
 
 
 def process_violation(violation, graph_client, ll_url, ws_id):
@@ -207,6 +204,8 @@ if __name__ == "__main__":
         "--accounts", help="Accounts list to iterate when creating the report", required=False)
     parser.add_argument(
         "--label", help="Filter compliance rules by using a label", required=False)
+    parser.add_argument(
+        "--stage", action="store_true")
     args = parser.parse_args()
     main(args.environment_sub_domain, args.environment_user_name, args.environment_password,
-         args.ws_name, args.compliance, args.accounts, args.label)
+         args.ws_name, args.compliance, args.accounts, args.label, args.stage)
