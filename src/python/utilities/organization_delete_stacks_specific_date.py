4d5a4aa9f8eadd8e2f06346e921c61b38f347aab
RoeeAgiman <roee@lightlytics.com>
2024-06-19 15:05:38 +0300

    organization_delete_stacks_specific_date.py: Added error handling #7

--- src/python/utilities/organization_delete_stacks_specific_date.py
+++ src/python/utilities/organization_delete_stacks_specific_date.py
@@ -3,6 +3,7 @@ import boto3
 import os
 import sys
 
+import concurrent.futures
 from datetime import datetime
 
 # Add the project root directory to the Python path
@@ -30,8 +31,8 @@ def main(aws_profile_name, accounts, control_role="OrganizationAccountAccessRole
     # Set up the STS client
     sts_client = boto3.client('sts')
 
-    # Get all activated regions from Org account
-    regions = [region['RegionName'] for region in boto3.client('ec2').describe_regions()['Regions']]
+    # Set region to find stacks in
+    region = "us-east-1"
 
     print("Fetching all accounts connected to the organization")
     list_accounts = get_all_accounts(org_client)
@@ -43,43 +44,57 @@ def main(aws_profile_name, accounts, control_role="OrganizationAccountAccessRole
     if accounts:
         sub_accounts = [sa for sa in sub_accounts if sa[0] in accounts]
 
-    for sub_account in sub_accounts:
-        try:
-            # Assume the role in the sub_account[0]
-            assumed_role = sts_client.assume_role(
-                RoleArn=f'arn:aws:iam::{sub_account[0]}:role/{control_role}',
-                RoleSessionName='MySessionName'
-            )
-        except Exception as e:
-            print(color(f"Account: {sub_account[0]} | Can't assume role, Error: {e}", "red"))
-            continue
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(delete_stacks, sts_client, sub_account, control_role, region, just_print)
+                   for sub_account in sub_accounts]
+        print(color(f"Number of threads created: {len(futures)}", "blue"))
+        completed_count = 0
+        for _ in concurrent.futures.as_completed(futures):
+            completed_count += 1
+            if completed_count % 50 == 0:
+                completed_percentage = int(completed_count / len(futures) * 100)
+                print(color(f"{completed_count} threads completed out of "
+                            f"{len(futures)} ({completed_percentage}%)", "blue"))
+        print(color("All threads completed", "green"))
 
-        print(color(f"Account: {sub_account[0]} | Initializing Boto session", "blue"))
-        sub_account_session = boto3.Session(
-            aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
-            aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
-            aws_session_token=assumed_role['Credentials']['SessionToken']
+
+def delete_stacks(sts_client, sub_account, control_role, region, just_print):
+    try:
+        # Assume the role in the sub_account[0]
+        assumed_role = sts_client.assume_role(
+            RoleArn=f'arn:aws:iam::{sub_account[0]}:role/{control_role}',
+            RoleSessionName='MySessionName'
         )
-        print(color(f"Account: {sub_account[0]} | Session initialized successfully", "green"))
+    except Exception as e:
+        print(color(f"Account: {sub_account[0]} | Can't assume role, Error: {e}", "red"))
+        return
 
-        try:
-            cft_client = sub_account_session.client("cloudformation", region_name="us-east-1")
-            list_stacks = cft_client.list_stacks()
-            cft_stacks = [stack for stack in list_stacks['StackSummaries'] if "TemplateDescription" in stack]
-            stream_stacks = [s for s in cft_stacks if 'lightlytics' in s['TemplateDescription'].lower()
-                             and 'ParentId' not in s
-                             and s['StackStatus'] == 'CREATE_COMPLETE'
-                             and s['CreationTime'].date() == datetime(2024, 3, 1).date()
-                             or s['CreationTime'].date() == datetime(2024, 2, 22).date()]
-            for s in stream_stacks:
-                if just_print:
-                    print(f"Account: {sub_account[0]} | Stack to be deleted: {s['StackName']}")
-                else:
-                    print(f"Account: {sub_account[0]} | Deleting {s['StackName']}")
-                    cft_client.delete_stack(StackName=s['StackName'])
-        except Exception as e:
-            print(color(f"Account: {sub_account[0]} | Error found on us-east-1, error: {e}"))
-            continue
+    print(color(f"Account: {sub_account[0]} | Initializing Boto session", "blue"))
+    sub_account_session = boto3.Session(
+        aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
+        aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
+        aws_session_token=assumed_role['Credentials']['SessionToken']
+    )
+    print(color(f"Account: {sub_account[0]} | Session initialized successfully", "green"))
+
+    try:
+        cft_client = sub_account_session.client("cloudformation", region_name=region)
+        list_stacks = cft_client.list_stacks()
+        cft_stacks = [stack for stack in list_stacks['StackSummaries'] if "TemplateDescription" in stack]
+        stream_stacks = [s for s in cft_stacks if 'lightlytics' in s['TemplateDescription'].lower()
+                         and 'ParentId' not in s
+                         and s['StackStatus'] == 'CREATE_COMPLETE'
+                         and s['CreationTime'].date() == datetime.date(2024, 3, 1)
+                         or s['CreationTime'].date() == datetime.date(2024, 2, 22)]
+        for s in stream_stacks:
+            if just_print:
+                print(f"Account: {sub_account[0]} | Stack to be deleted: {s['StackName']}")
+            else:
+                print(f"Account: {sub_account[0]} | Deleting {s['StackName']}")
+                cft_client.delete_stack(StackName=s['StackName'])
+    except Exception as e:
+        print(color(f"Account: {sub_account[0]} | Error found on us-east-1, error: {e}", "red"))
+        return
 
 
 if __name__ == "__main__":
