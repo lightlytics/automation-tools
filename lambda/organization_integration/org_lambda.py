c5f89d704838ca2841b23c3a6f457c25d0ac8656
Dotan Alter <dotan@stream.security>
2025-04-28 10:02:16 +0300

    add eks audit logs to organization lambda

--- lambda/organization_integration/org_lambda.py
+++ lambda/organization_integration/org_lambda.py
@@ -18,6 +18,8 @@ parser.add_argument("--control-role", default="OrganizationAccountAccessRole", h
 parser.add_argument("--response", action="store_true", help="Enable creation of the response stack.")
 parser.add_argument("--response-region", default="us-east-1", help="Region for response stack.")
 parser.add_argument("--response-exclude-runbooks", help="Comma separated list of runbooks to exclude from response stack.")
+parser.add_argument("--eks-audit-logs", action="store_true", help="Enable creation of the EKS audit logs.")
+parser.add_argument("--eks-audit-logs-regions", required=False, help="Comma separated list of regions to enable EKS audit logs.")
 args = parser.parse_args()
 
 iam_client = boto3.client('iam')
@@ -145,13 +147,17 @@ def main():
         "PARALLEL": "8",
         "CONTROL_ROLE": args.control_role,
         "RESPONSE": str(args.response).lower(),
-        "RESPONSE_REGION": args.response_region
+        "RESPONSE_REGION": args.response_region,
+        "EKS_AUDIT_LOGS": str(args.eks_audit_logs).lower(),
     }
     if args.accounts is not None:
         env_vars["ACCOUNTS"] = args.accounts
         
     if args.response_exclude_runbooks is not None:
         env_vars["RESPONSE_EXCLUDE_RUNBOOKS"] = args.response_exclude_runbooks
+
+    if args.eks_audit_logs_regions is not None:
+        env_vars["EKS_AUDIT_LOGS_REGIONS"] = args.eks_audit_logs_regions
 
     with open(zip_filename, 'rb') as f:
         zipped_code = f.read()
